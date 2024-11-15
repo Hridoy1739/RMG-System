e92c76ddb5e51724a2952179d029c72995bbc6de
Hridoy1739 <170397455+Hridoy1739@users.noreply.github.com.>
2024-11-15 14:32:25 +0600

    Primary implementation of all attributes and methods of Garments class

--- src/Garment.java
+++ src/Garment.java
@@ -1,4 +1,21 @@
 public class Garment {
 
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
 
+
+    void updateStock(int quantity){
+
+    }
+
+
+    double calculateDiscountPrice(double discountPercentage){
+
+
+    }
 }
