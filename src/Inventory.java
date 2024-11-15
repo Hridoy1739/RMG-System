53308b34cb2e9553f3e7cafdce64eb122b738293
Hridoy1739 <170397455+Hridoy1739@users.noreply.github.com.>
2024-11-15 15:07:45 +0600

    Attributes and Methods implemented in Inventory class.

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,17 @@
-public class Inventory {
+import java.util.List;
 
+public class Inventory {
+    public List<Garment> garments;
+
+
+    void addGarment(Garment garment){
+
+    }
+    void removeGarment(String id){
+
+    }
+    Garment findGarment(String id){
+
+    }
 }
 
