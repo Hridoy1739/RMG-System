c50ae0f0651baf1870864ee8fee8a3e08893d824
Hridoy1739 <170397455+Hridoy1739@users.noreply.github.com.>
2024-11-16 19:54:22 +0600

    Initialize Garments Object using Constructor and set values in main functions object

--- src/Garment.java
+++ src/Garment.java
@@ -8,14 +8,23 @@ public class Garment {
     public double price;
     public int stockQuantity;
 
+    public Garment(String id, String name, String description, String size, String color, double price, int stockQuantity){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
 
     void updateStock(int quantity){
-
+        this.stockQuantity += quantity;
     }
 
 
     double calculateDiscountPrice(double discountPercentage){
-
-
+        return price - (price * discountPercentage / 100);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
-System.out.printf("Hello and welcome!");
-
+        Garment dekoGarment = new Garment("number1","T-shirt", "Polo T-shirt","Availabe S,M,L, XL","Red, Gree, Blue, Black","500.00","100");
+dekoGarment.updateStock(50);
+        System.out.println("After 2nd shifting, Quantity is: "+dekoGarment.stockQuantity);
+        double discountedPrice = dekoGarment.calculateDiscountPrice(10);
+        System.out.println("Discount Price: "+ discountedPrice);
     }
 }
